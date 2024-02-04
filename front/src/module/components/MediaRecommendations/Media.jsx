206d13b7812aa0f6d5a7ed533914226beda1e68b
Gargee-Bhowmick <gargee.bhowmick09@gmail.com>
2024-02-04 09:43:06 +0530

    Media setup

--- front/src/module/components/MediaRecommendations/Media.jsx
+++ front/src/module/components/MediaRecommendations/Media.jsx
@@ -1,21 +1,24 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import { Grid, Typography } from "@mui/material";
 import ReactPlayer from "react-player/lazy";
 
 const Media = () => {
-  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [selectedVideo, setSelectedVideo] = useState(0);
 
-  const handleVideoClick = (videoUrl) => {
-    setSelectedVideo(videoUrl);
+  const handleVideoClick = (index) => {
+    setSelectedVideo(index);
   };
 
   const videoList = [
     "https://www.youtube.com/watch?v=oUFJJNQGwhk",
     "https://www.youtube.com/watch?v=jNgP6d9HraI",
+    "https://www.youtube.com/watch?v=oUFJJNQGwhk",
     "https://www.youtube.com/watch?v=jNgP6d9HraI",
+    "https://www.youtube.com/watch?v=oUFJJNQGwhk",
     "https://www.youtube.com/watch?v=jNgP6d9HraI",
+    "https://www.youtube.com/watch?v=oUFJJNQGwhk",
     "https://www.youtube.com/watch?v=jNgP6d9HraI",
-    "https://www.youtube.com/watch?v=jNgP6d9HraI",
+    "https://www.youtube.com/watch?v=oUFJJNQGwhk",
     "https://www.youtube.com/watch?v=jNgP6d9HraI",
     // Add more video URLs as needed
   ];
@@ -26,20 +29,17 @@ const Media = () => {
         <Typography variant="h5" gutterBottom>
           Media
         </Typography>
-        <ReactPlayer url={videoList[0]} width="100%" height="100%" />
+        <ReactPlayer url={videoList[selectedVideo]} width="100%" height="100%" />
       </Grid>
       <Grid item xs={12} lg={4} md={6}>
         <Grid container spacing={2}>
-          {videoList.slice(1).map((videoUrl, index) => (
-            <React.Fragment key={videoUrl}>
-              <Grid item xs={12} lg={6}>
-                <ReactPlayer
-                  className="react-player"
-                  url={videoUrl}
-                  width="100%"
-                  height="100%"
-                />
-              </Grid>
+          {videoList.map((videoUrl, index) => (
+            <React.Fragment key={videoUrl} onClick={()=>{setSelectedVideo(index)}}>
+              {index !== selectedVideo && (
+                <Grid item xs={12} lg={6} onClick={() => handleVideoClick(index)}>
+                  <ReactPlayer className="react-player" url={videoUrl} width="100%" height="100%" />
+                </Grid>
+              )}
               <Grid item xs={12} lg={6}>
                 <Typography variant="body1" gutterBottom>
                   Video Description {index + 1}
